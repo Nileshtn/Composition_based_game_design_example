caf6b8d9b9141d82537ec56939d26d54c2384223
Nileshtn <tjnilesh55gmail.com>
2024-09-24 16:43:52 +0530

    added sound for hammer hit and coin collection

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,7 +1,10 @@
 extends Area2D
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
 	print("coin collected!")
 	if body.has_node("player_stats"):
 		body.get_node("player_stats").add_coin_count(1)
-	queue_free()
+	
+	animation_player.play("on_coin_collected")
+	
